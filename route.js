b5650578c6ac3392a44bec27861d2129dba49939
TheKonamiCode <62217655+TheKonamiCode@users.noreply.github.com>
2021-09-10 21:40:37 -0400

    Retrait commentaire inutile

--- route.js
+++ route.js
@@ -1,33 +1,16 @@
 const http = require('http');
 const url = require('url');
 
-// Matière à discussion
-// https://stackoverflow.com/questions/21001455/should-a-rest-api-be-case-sensitive-or-non-case-sensitive
-// https://blog.restcase.com/5-basic-rest-api-design-guidelines/
-
 module.exports = http.createServer((req, res) => {
   var controller = require('./controller.js');
-  const reqUrl =  url.parse(req.url, true);
+  const reqUrl = url.parse(req.url, true);
   const pathName = reqUrl.pathname;
-  if( pathName == '/math' && req.method === 'GET') {
+  if (pathName == '/math' && req.method === 'GET') {
 
     controller.math(req, res);
   }
- else {
-  console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
-  controller.invalidUrl(req, res);
-}
-  // // GET endpoint
-  // else if(reqUrl.pathname == '/getCours' && req.method === 'GET') {
-  //   console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
-  //   coursOps.getCours(req, res);
-  // // POST endpoint
-  // } else if(reqUrl.pathname == '/createCours' && req.method === 'POST') {
-  //   console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
-  //   coursOps.createCours(req, res);
-  // // URL invalide
-  // } else {
-  //   console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
-  //   coursOps.invalidUrl(req, res);
-  // }
+  else {
+    console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
+    controller.invalidUrl(req, res);
+  }
 })
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const hostname = '127.0.0.1';
-const port = 2357; // mettons
+const port = 2357;
 const server = require('./route.js'); // importer les routes
 server.listen(port, hostname, () => {
   console.log('Serveur en exécution sur http://' + hostname + ':' + port + '/');

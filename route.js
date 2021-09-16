56e447476f0321afa381bd7926525ef5e4ce59b9
TheKonamiCode <62217655+TheKonamiCode@users.noreply.github.com>
2021-09-16 16:57:19 -0400

    t

--- route.js
+++ route.js
@@ -2,15 +2,35 @@ const http = require('http');
 const url = require('url');
 
 module.exports = http.createServer((req, res) => {
-  var controller = require('./controller.js');
-  const reqUrl = url.parse(req.url, true);
-  const pathName = reqUrl.pathname;
-  if (pathName == '/api/maths' && req.method === 'GET') {
 
-    controller.math(req, res);
+  AccessControlConfig(res);
+  if (!Prefligth(req, res)) {
+    var controller = require('./controller.js');
+    const reqUrl = url.parse(req.url, true);
+    const pathName = reqUrl.pathname;
+    if (pathName == '/api/maths' && req.method === 'GET') {
+
+      controller.math(req, res);
+    }
+    else {
+      console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
+      controller.invalidUrl(req, res);
+    }
   }
-  else {
-    console.log('Request type: ' + req.method + ' Endpoint: ' + req.url);
-    controller.invalidUrl(req, res);
+});
+
+function AccessControlConfig(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+}
+function Prefligth(req, res) {
+  if (req.method === 'OPTIONS') {
+      console.log('preflight CORS verifications');
+      res.end();
+      // request handled
+      return true;
   }
-})
+  // request not handled
+  return false;
+}
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const hostname = '127.0.0.1';
 const port = 5000;
 const server = require('./route.js'); // importer les routes
+//const PORT = process.env.PORT || 5000;
+// require('http').createServer((req, res) => {
+//   console.log("allo");
+
+//   ShowRequestInfo(req);
+
+//   if(true || !CORS_Prefligth(req, res)) {
+//     if (!API_Endpoint(req, res)) {
+//       notFound(res);
+//     }
+//   }
+// })
 server.listen(port, hostname, () => {
   console.log('Serveur en exécution sur http://' + hostname + ':' + port + '/');
 });
